c0eac055b820d220d2eab7a37b43c49c00e17102
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-03-01 15:35:48 +0300

    Bug fix audio not defined

--- app/scanner/page.tsx
+++ app/scanner/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/navigation";
 
 const ScannerPage = () => {
   const router = useRouter();
+  if (typeof window === "undefined") return null;
+
   return (
     <div className="flex flex-col h-screen items-center justify-center">
       <QrScanner
